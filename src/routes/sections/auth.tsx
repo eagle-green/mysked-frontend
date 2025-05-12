6b22932e9aa1e80aaa87c2d80732dbd3bd52a590
kiwoonjung <kiwoon0627@gmail.com>
2025-05-12 13:29:59 -0700

    first commit

---
+++ src/routes/sections/auth.tsx
@@ -0,0 +1,60 @@
+import type { RouteObject } from 'react-router';
+
+import { Outlet } from 'react-router';
+import { lazy, Suspense } from 'react';
+
+import { AuthCenteredLayout } from 'src/layouts/auth-centered';
+
+import { SplashScreen } from 'src/components/loading-screen';
+
+import { GuestGuard } from 'src/auth/guard';
+
+// ----------------------------------------------------------------------
+
+/** **************************************
+ * Jwt
+ *************************************** */
+const Jwt = {
+  SignInPage: lazy(() => import('src/pages/auth/jwt/sign-in')),
+  SignUpPage: lazy(() => import('src/pages/auth/jwt/sign-up')),
+};
+
+const authJwt = {
+  path: 'jwt',
+  children: [
+    {
+      path: 'sign-in',
+      element: (
+        <GuestGuard>
+          <AuthCenteredLayout>
+            <Jwt.SignInPage />
+          </AuthCenteredLayout>
+        </GuestGuard>
+      ),
+    },
+    {
+      path: 'sign-up',
+      element: (
+        <GuestGuard>
+          <AuthCenteredLayout>
+            <Jwt.SignUpPage />
+          </AuthCenteredLayout>
+        </GuestGuard>
+      ),
+    },
+  ],
+};
+
+// ----------------------------------------------------------------------
+
+export const authRoutes: RouteObject[] = [
+  {
+    path: 'auth',
+    element: (
+      <Suspense fallback={<SplashScreen />}>
+        <Outlet />
+      </Suspense>
+    ),
+    children: [authJwt],
+  },
+];
