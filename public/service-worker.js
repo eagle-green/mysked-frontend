2a948a66dd5bde87c5d3223e651fea7c4030d88a
kiwoonjung <kiwoon0627@gmail.com>
2025-10-18 23:10:36 -0700

    chore: bump version to 1.0.13

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // Increment this version number whenever you deploy updates
-const APP_VERSION = '1.0.12';
+const APP_VERSION = '1.0.13';
 const CACHE_NAME = `mysked-${APP_VERSION}`;
 const urlsToCache = [
   '/',
