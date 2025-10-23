94553fc11a9eb4060b428729c21d3115bcf96ba5
Kiwoon <110749974+kiwoonjung@users.noreply.github.com>
2025-10-23 11:44:33 -0700

    Chore/version test (#156)
    * chore: bump version to 1.1.5
    
    * chore: bump version to 1.1.6
    
    * chore: bump version to 1.1.7
    
    * chore: bump version to 1.1.8
    
    * update
    
    * fixed build error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // Increment this version number whenever you deploy updates
-const APP_VERSION = '1.1.4';
+const APP_VERSION = '1.1.8';
 const CACHE_NAME = `mysked-${APP_VERSION}`;
 const urlsToCache = [
   '/',
